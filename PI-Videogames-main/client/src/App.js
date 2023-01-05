02782c429f7880727e952071f3d633db47e6d771
joaxd69 <joakig6@gmail.com>
2023-01-05 20:35:22 -0300

    back end

--- PI-Videogames-main/client/src/App.js
+++ PI-Videogames-main/client/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Home from './components/home/home';
-import {BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Landingpage from './components/Landing page/Landingpage';
 import Game from './components/game/Game';
 import Results from './components/search results/Results';
 import Nav from './components/Nav/Nav';
 import Creategame from './components/Creategame/Creategame';
 import Favourites from './components/Favourites/Favourites';
+import GamesCreated from './components/GamesCreated/GamesCreated';
 import EditGame from './components/EditGame/EditGame'
-
 import axios from 'axios'
 axios.defaults.baseURL ='http://localhost:3001/';
 function App() {
   return (
   <BrowserRouter>
+   
+    
      <div className="App">    
       
       <Route path='/' render={()=>window.location.pathname!=='/'?<Nav/>:null}/>
@@ -23,7 +25,7 @@ function App() {
       <Route exact path='/Favourites' component={Favourites}/>
       <Route path='/details' component={Game}/>
       <Route exact path='/creategame' component={Creategame}/>
-     
+      <Route exact path='/gamescreated' component = {GamesCreated}/>
       <Route  path='/editgame' component={EditGame}/>
      </div>
    
