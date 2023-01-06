c19dd71c5f088c60f10f914c4731a0b64fb06341
joaxd69 <joakig6@gmail.com>
2023-01-05 21:34:05 -0300

    hola

--- PI-Videogames-main/api/index.js
+++ PI-Videogames-main/api/index.js
@@ -19,11 +19,15 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
+const {
+  PORT=3001
+} = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {/////force true resetea los datos de la base de datos
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
 
